fb8c1ff7c7761a9735d6421078f756a4f03d9dfc
Basil Regi <08regib@googlemail.com>
2022-10-04 23:39:47 +0100

    Fix issues in creating PWA

--- js/main.js
+++ js/main.js
@@ -2,9 +2,6 @@ window.onload = () => {
   'use strict';
 
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker
-      .register('./sw.js');
-      .then(res => console.log("Service Worker registered"))
-      .catch(err => console.log("Unable to register Service Worker", err))
+    navigator.serviceWorker.register('./sw.js');
   }
 }
--- sw.js
+++ sw.js
@@ -3,21 +3,22 @@ var assets = [
   '/',
   '/index.html',
   '/css/style.css'
-  'js/main.js'
+  '/js/main.js'
 ];
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(cacheName).then(cache => {
-      cache.addAll(assets)
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(assets);
     })
-  )
-})
+  );
+  self.skipWaiting();
+});
 
-self.addEventListener("fetch", fetchEvent => {
-  fetchEvent.respondWith(
-    caches.match(fetchEvent.request).then(response => {
-      return response || fetch(fetchEvent.request)
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
     })
-  )
-})
+  );
+});
